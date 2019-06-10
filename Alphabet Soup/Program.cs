64080c08b460f0c4567a4453a94847276b9799ae
Sross89 <43963661+Sross89@users.noreply.github.com>
2019-06-10 11:35:46 -0600

    Add files via upload

---
+++ Alphabet Soup/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+// Basic program to scramble input strings
+namespace Alphabet_Soup
+{
+    class Program
+    {
+        public static string AlphabeticalSort(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+
+        public static string AlphabeticalSortBackwards(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            Array.Reverse(characters);
+            return new string(characters);
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Enter String: ");
+            input = Console.ReadLine();
+            while (input != "0")
+            {
+                Console.WriteLine("Sort: " + AlphabeticalSort(input));
+                Console.WriteLine("Sort Backwards: " + AlphabeticalSortBackwards(input));
+                Console.Write("\nEnter String: ");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
