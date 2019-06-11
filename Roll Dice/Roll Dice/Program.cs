4b7a68fff7c87dc4c73d79ec3e44002396ab34ef
Sross89 <43963661+Sross89@users.noreply.github.com>
2019-06-11 11:50:11 -0600

    Add files via upload

---
+++ Roll Dice/Roll Dice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Roll_Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                Console.WriteLine("Dice 1: " + dice1);
+                Console.WriteLine("Dice 2: " + dice2 + "\n");
+            }
+        }
+    }
+}
